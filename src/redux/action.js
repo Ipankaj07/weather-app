a8a74376564ed0beae32dfeb56588e01cce4706f
Pankaj Raj <75722183+Ipankaj07@users.noreply.github.com>
2022-07-24 15:43:40 +0530

    Fixes Error Of Getting forcast Data

--- src/redux/action.js
+++ src/redux/action.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 import { GET_REPORT_LOADING, GET_REPORT_SUCCESS, GET_REPORT_FAILURE } from '../constant/weatherActionType';
 
 const weatherApiKey = process.env.REACT_APP_WEATHER_API_KEY;
+// const weatherApiKey = "b9f8f8f8f8f8f8f8f8f8f8f8f8f8f8f8";
 
 const getReportLoading = () => {
     return {
@@ -25,13 +26,24 @@ const getReportFailure = () => {
 const getReport = (data) => async (dispatch) => {
     dispatch(getReportLoading());
     try {
-        const city = data.city;
-        console.log(city, "im here from action"); 
-        const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${weatherApiKey}`);
-        dispatch(getReportSuccess(response.data));
+        const city = data;
+        let dailyData = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${weatherApiKey}`);
+        dailyData = dailyData.data;
+        let { lon, lat } = dailyData.coord;
+        let forcastData = await axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=hourly,minutely&units=metric&appid=${weatherApiKey}`);
+        forcastData = forcastData.data.daily;
+
+        let reportData = {
+            dailyData,
+            forcastData
+        }
+
+        dispatch(getReportSuccess(reportData));
+        // console.log(reportData, "im here from action");
     } catch (error) {
         dispatch(getReportFailure());
     }
 }
+
 
 export { getReport };
